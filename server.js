83b9f013abd92b1c3d811bfe2cb01e9b5ff27028
Robert Kawecki <robert+github@rkaw.pl>
2018-02-25 03:13:59 +0100

    Add a /status endpoint for monitoring
    For #1

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ const express = require('express');
 // Init:
 const app = express();
 app.use(express.static('public'));
+app.get('/status', function(req, res) {
+  res.send({ ok: true });
+});
 app.listen(process.env.HTTP_PORT || 3000);
