e7d7761f079b4c399613155090a5e64108819ffd
Robert Kawecki <robert+github@rkaw.pl>
2018-02-25 03:48:24 +0100

    Add socket.io
    This patch adds a dependency on the socket.io package and enables its use in the HTTP server.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 'use strict';
 
 // Requires:
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 // Init:
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 app.use(express.static('public'));
 app.get('/status', function(req, res) {
   res.send({ ok: true });
 });
-app.listen(process.env.HTTP_PORT || 3000);
+server.listen(process.env.HTTP_PORT || 3000);
